3e82b2e88e7cbe3da15880de15e9e8c2087bd812
Mashiro120 <120540590@qq.com>
2019-01-07 14:58:35 +0800

    origin type

---
+++ thulearnscrapy.py
@@ -0,0 +1,12 @@
+'''
+This project is to download all the files in the learn.tsinghua.edu.cn easily.
+Everyone is free to use it.
+GPL license.
+Poor English [=,=]
+'''
+import requests
+userid="bxl15"
+userpass="BAOZixl123"
+folder="./"
+url="https://learn.tsinghua.edu.cn/MultiLanguage/lesson/teacher/loginteacher.jsp"
+useragent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3642.0 Safari/537.36"
